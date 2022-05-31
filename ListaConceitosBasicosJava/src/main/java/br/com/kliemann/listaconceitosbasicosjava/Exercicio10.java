d7cf5e2e32772ed31b89f67bef8e1b82504d9901
Guilherme Kliemann <91554258+guilhermekliemann@users.noreply.github.com>
2022-05-30 22:32:18 -0300

    Update Exercicio10.java

--- ListaConceitosBasicosJava/src/main/java/br/com/kliemann/listaconceitosbasicosjava/Exercicio10.java
+++ ListaConceitosBasicosJava/src/main/java/br/com/kliemann/listaconceitosbasicosjava/Exercicio10.java
@@ -11,7 +11,12 @@ public class Exercicio10 {
         verifique se esse nome está no vetor. 
         */
         
-        //INCOMPLETO - falta colocar animal nao foi encontrado
+        String[] animais = {"Cachorro","Gato","Peixe","Leão","Onça","Tucano","Flamingo","Tubarão","Vaca","Porco"};
+
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.println("Digite o nome de um animal para verificar se ele está presente no vetor:");
+        String nomeAnimal = sc.next();
         
         String[] animais = {"Cachorro","Gato","Peixe","Leão","Onça","Tucano","Flamingo","Tubarão","Vaca","Porco"};
 
@@ -20,17 +25,21 @@ public class Exercicio10 {
         System.out.println("Digite o nome de um animal para verificar se ele está presente no vetor:");
         String nomeAnimal = sc.next();
         
-        verificaAnimal(animais, nomeAnimal);
+        System.out.println(verificaAnimal(animais, nomeAnimal));
         
     }
     
-    private static void verificaAnimal(String[] animais, String nomeAnimal) {
+    private static String verificaAnimal(String[] animais, String nomeAnimal) {
+        
+        String retorno = "Animal não encontrado no vetor!";
         
         for(int i=0; i<animais.length; i++) {
             if(animais[i].equals(nomeAnimal)) {
-                System.out.println(animais[i] + " encontrado na posição " + i + " do vetor!");
+                retorno = animais[i] + " encontrado na posição " + i + " do vetor!";
             }
         }
+        
+        return retorno;
         
     }
     
